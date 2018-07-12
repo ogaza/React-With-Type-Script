0f7a7d2cf422543abbe785f96a34c9af31c5bbfe
olaf.gaza <olaf.gaza@applandeo.com>
2018-07-12 13:30:35 +0200

    adding some comments to the webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 module.exports = {
+    // a string here because there is one file as an entry point
+    // if there is more than one, then use an array
     entry: './src/app.tsx',
+    // the path and name of the file that will be generated, and to be referenced in the html file
     output: {
         path: __dirname + '/public',
         filename: 'build/app.js'
     },
     resolve: {
+        // by default Webpack does no load .ts and .tsx files so it needs to be told
         extensions: ['.ts', '.tsx', '.js']
     },
+    // set up the loader responsible for transpiling the code
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: 'ts-loader' }
+            { 
+                // a regular expression that tests what kind of files to run through this loader
+                test: /\.tsx?$/,
+                loader: 'ts-loader' 
+            }
         ]
     }
 }
