a98b32c69bd902af453d340fe6f997606bfaee4b
olaf.gaza <olaf.gaza@applandeo.com>
2018-07-12 13:33:13 +0200

    small app.tsx refactor

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Hello from './components/hello';
 
+const App = () => (
+    <div>
+      <Hello name="Type Script Application"/>
+    </div>
+  );
 
 ReactDOM.render(
-    <Hello name="TypeScript"/>,
+    <App/>,
     document.getElementById("root")
 );
