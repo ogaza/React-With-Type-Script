87daebbc49130768a6a2e5811ee6f4cdf33233c2
olaf.gaza <olaf.gaza@applandeo.com>
2018-07-16 13:02:47 +0200

    style, css, less loaders, ant design library

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,51 @@
 var path = require('path');
 
 module.exports = {
-    resolve: {
-        alias: {
-            _src: path.resolve(__dirname, '/src')
-        },
-        extensions: ['*', '.js', '.jsx', ".ts", ".tsx", '.css', '.less', '.png', '.svg', '.json']
+  resolve: {
+    alias: {
+      _src: path.resolve(__dirname, '/src')
     },
-    // a string here because there is one file as an entry point
-    // if there is more than one, then use an array
-    entry: './src/app.tsx',
-    // the path and name of the file that will be generated, and to be referenced in the html file
-    output: {
-        path: __dirname + '/public',
-        filename: 'build/app.js'
-    },
-    resolve: {
-        // by default Webpack does no load .ts and .tsx files so it needs to be told
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    // set up the loader responsible for transpiling the code
-    module: {
-        rules: [
-            { 
-                // a regular expression that tests what kind of files to run through this loader
-                test: /\.tsx?$/,
-                loader: 'ts-loader' 
+    extensions: ['*', '.js', '.jsx', ".ts", ".tsx", '.css', '.less', '.png', '.svg', '.json']
+  },
+  // a string here because there is one file as an entry point
+  // if there is more than one, then use an array
+  entry: './src/app.tsx',
+  // the path and name of the file that will be generated, and to be referenced in the html file
+  output: {
+    path: __dirname + '/public',
+    filename: 'build/app.js'
+  },
+  resolve: {
+    // by default Webpack does no load .ts and .tsx files so it needs to be told
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  // set up the loader responsible for transpiling the code
+  module: {
+    rules: [{
+        // a regular expression that tests what kind of files to run through this loader
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
             }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              sourceMap: true,
+            }
+          }
         ]
-    }
+      }
+    ]
+  }
 }
