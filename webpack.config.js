f36df43b93d6047001d19048f4727ac0e7a17f58
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-03-06 13:16:33 +0100

    postCss fix

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
           {
             loader: 'postcss-loader',
             options: {
-              sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment
+              sourceMap: true
             }
           }
         ]
