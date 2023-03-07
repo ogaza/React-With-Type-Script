32f908c21f4e1ebfc6c628bcf10b29aee1b3ee6b
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-03-07 14:41:00 +0100

    cleanup

--- src/toDos/sagas/toDos.ts
+++ src/toDos/sagas/toDos.ts
@@ -1,4 +1,4 @@
-import { call, put, takeEvery, select } from 'redux-saga/effects';
+import { put, takeEvery, select } from 'redux-saga/effects';
 import { actionTypes } from '../actions/actionTypes';
 
 function* removeToDo(action) {
