de84a1c9fddb51fbc4632578e124545360c3945c
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-03-08 12:59:56 +0100

    TodoList and AddTodoForm

--- src/layout/MainPanelContainer.tsx
+++ src/layout/MainPanelContainer.tsx
@@ -4,6 +4,7 @@ import { Actions } from '../toDos/actions/actionCreators';
 import { useSelector } from 'react-redux';
 import { IAppState } from '../store/state';
 import './MainPanelContainer.scss';
+import { useState } from 'react';
 
 export default function MainPanelContainer() {
   const todos = useSelector((state: IAppState) => state.todos);
@@ -12,29 +13,70 @@ export default function MainPanelContainer() {
   return (
     <section className="main-panel">
       This is main panel
-      <button
-        onClick={() => {
-          addToDo();
-        }}
-      >
-        toggle
-      </button>
-      {todos.todos.map((x) => (
-        <div key={x.id}>
-          <span>{x.text}</span>
-          <span>
-            <button onClick={() => deleteToDo(x.id)}>delete</button>
-          </span>
-        </div>
-      ))}
+      <AddTodoForm onSubmit={addToDo} />
+      <TodoList todos={todos} onDelete={deleteToDo} />
     </section>
   );
 
-  function addToDo() {
-    dispatch(Actions.addToDo('test'));
+  function addToDo(text) {
+    dispatch(Actions.addToDo(text));
   }
 
   function deleteToDo(id) {
     dispatch(Actions.deleteToDo(id));
+  }
+}
+
+function AddTodoForm({ onSubmit }) {
+  const [text, setText] = useState('');
+  return (
+    <>
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleSubmit}>create</button>
+    </>
+  );
+
+  function handleSubmit() {
+    if (!text) {
+      console.log('invalid data: empty todo text');
+      return;
+    }
+
+    onSubmit(text);
+    setText('');
+  }
+
+  function handleChange(e) {
+    const value = e.target.value;
+    console.log(e.target.value);
+    setText(value);
+  }
+}
+
+function TodoList({ todos, onDelete }) {
+  return (
+    <>
+      {todos.todos.map((x) => (
+        <div key={x.id}>
+          <span>{x.text}</span>
+          <span>
+            <button data-id={x.id} onClick={handleDeleteClick}>
+              delete
+            </button>
+          </span>
+        </div>
+      ))}
+    </>
+  );
+
+  function handleDeleteClick(e) {
+    const {
+      target: { dataset: { id: stingId = undefined } = {} }
+    } = e;
+
+    const id = Number(stingId);
+    if (!id) return;
+
+    onDelete(id);
   }
 }
