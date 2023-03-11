a8bade84e0b6fde628ec5a84800ee3c020857897
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-03-12 00:58:52 +0100

    add item as item buttons

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -4,29 +4,61 @@ import { SubmitButton } from './SubmitButton';
 import './AddItemForm.scss';
 
 export function AddItemForm({ onSubmit, enabled }) {
-  const [text, setText] = useState('');
-  // const isValid = true;
-  const isValid = !!text;
-  const canSubmit = isValid && enabled;
+  const items = [
+    { id: 1, text: 'item 1' },
+    { id: 2, text: 'item 2' },
+    { id: 3, text: 'item 3' }
+  ];
 
   return (
     <section className="add-item">
-      <h3>Add item</h3>
-      <input type="text" value={text} onChange={handleChange} disabled={!enabled} />
-      <SubmitButton onClick={handleSubmit} label="create" enabled={canSubmit} />
+      <h3>Add Item</h3>
+      {items.map((item) => {
+        return (
+          <SubmitButton
+            key={item.id}
+            onClick={() => handleSubmit(item.text)}
+            label={item.text}
+            enabled={enabled}
+          />
+        );
+      })}
+      {/* <input type="text" value={text} onChange={handleChange} disabled={!enabled} /> */}
     </section>
   );
 
-  function handleSubmit() {
-    if (canSubmit) {
+  function handleSubmit(text) {
+    if (enabled) {
       onSubmit(text);
-      setText('');
       return;
     }
   }
-
-  function handleChange(e) {
-    const value = e.target.value;
-    setText(value);
-  }
 }
+
+// export function AddItemForm({ onSubmit, enabled }) {
+//   const [text, setText] = useState('');
+//   // const isValid = true;
+//   const isValid = !!text;
+//   const canSubmit = isValid && enabled;
+
+//   return (
+//     <section className="add-item">
+//       <h3>Add item</h3>
+//       <input type="text" value={text} onChange={handleChange} disabled={!enabled} />
+//       <SubmitButton onClick={handleSubmit} label="create" enabled={canSubmit} />
+//     </section>
+//   );
+
+//   function handleSubmit() {
+//     if (canSubmit) {
+//       onSubmit(text);
+//       setText('');
+//       return;
+//     }
+//   }
+
+//   function handleChange(e) {
+//     const value = e.target.value;
+//     setText(value);
+//   }
+// }
