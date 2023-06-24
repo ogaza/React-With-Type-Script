4437f1505935e1365c698da9b3e9bc68e18af1b2
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-06-24 20:05:52 +0200

    status bar - clock and app menu button

--- src/layout/StatusBar.tsx
+++ src/layout/StatusBar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import './StatusBar.scss';
 import { toggleAdminPanel } from '../adminPanel/actions';
 import { useDispatch } from 'react-redux';
@@ -9,18 +10,55 @@ export default function StatusBar() {
   return (
     <div className="status-bar">
       <section className="status-bar__info">
-        <div>Simple POS</div>
+        <div>POS</div>
+        <Clock />
       </section>
       <section className="status-bar__items"></section>
       <section className="status-bar__actions">
-        <div role="button" onClick={handleClick}>
-          Menu
-        </div>
+        <AppMenuButton onClick={handleMenuButtonClick} />
       </section>
     </div>
   );
 
-  function handleClick() {
+  function handleMenuButtonClick() {
     dispatch(toggleAdminPanel());
   }
+}
+
+function AppMenuButton({ onClick }) {
+  return (
+    <div role="button" className="app-menu-button" onClick={handleClick}>
+      <div className="squares">
+        <div className="square__container">
+          <div className="square"></div>
+        </div>
+        <div className="square__container">
+          <div className="square"></div>
+        </div>
+        <div className="square__container">
+          <div className="square"></div>
+        </div>
+        <div className="square__container">
+          <div className="square"></div>
+        </div>
+      </div>
+    </div>
+  );
+
+  function handleClick() {
+    onClick();
+  }
+}
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>{time.toLocaleTimeString()}</div>;
 }
