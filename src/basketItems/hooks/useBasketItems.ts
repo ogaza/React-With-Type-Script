125c0795f62ac2d02c08f71cee2d263d6c97d69a
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-07-05 14:30:02 +0200

    define addBasketItem method in the useBasketItems hook

--- src/basketItems/hooks/useBasketItems.ts
+++ src/basketItems/hooks/useBasketItems.ts
@@ -1,5 +1,7 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { IAppState } from '../../application/store/state';
+import { actions as basketItemsActions } from '../../basketItems';
+import { useBaskets } from '../../baskets';
 
 export function useBasketItems() {
   // const [basketItems, setBasketItems] = useState([
@@ -7,6 +9,10 @@ export function useBasketItems() {
   //   { id: 2, name: 'Item 2', price: 14.99, quantity: 1 },
   //   { id: 3, name: 'Item 3', price: 4.99, quantity: 1 }
   // ]);
+  const dispatch = useDispatch();
+  const { getSelectedBasket } = useBaskets();
+  const { id: selectedBasketId } = getSelectedBasket() || {};
+
   const { state, collection: basketItems } = useSelector(
     (state: IAppState) => state.basketItems
   ) || {
@@ -14,7 +20,7 @@ export function useBasketItems() {
     collection: []
   };
 
-  return { getBasketItems, changeBasketItem };
+  return { getBasketItems, changeBasketItem, addBasketItem };
 
   function getBasketItems() {
     return basketItems.map((x) => ({ ...x }));
@@ -25,6 +31,42 @@ export function useBasketItems() {
   //     value: (x.price * x.quantity).toFixed(2)
   //   }));
   // }
+
+  function addBasketItem({ id, name, price }) {
+    // console.log('useBasketItems:addBasketItem', { id, name, price });
+    const { id: selectedBasketId } = getSelectedBasket() || {};
+
+    const existingBasketItem = basketItems.find(
+      (x) => x.name === name && x.basketId === selectedBasketId
+    );
+
+    if (!existingBasketItem) {
+      dispatch(
+        basketItemsActions.addItem({
+          id,
+          name,
+          price,
+          quantity: 1,
+          basketId: selectedBasketId
+        })
+      );
+
+      return;
+    }
+    console.log('existingBasketItem', existingBasketItem);
+
+    const newQuantity = existingBasketItem.quantity + 1;
+
+    dispatch(
+      basketItemsActions.editItem({
+        id,
+        name,
+        price,
+        quantity: newQuantity,
+        basketId: selectedBasketId
+      })
+    );
+  }
 
   function changeBasketItem(changedItem) {
     if (changedItem.quantity < 0) {
