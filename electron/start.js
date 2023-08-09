6c221ca44182dcf8c6061b10c0056c914ca07a06
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-08-09 11:29:14 +0200

    load app to the main window

--- electron/start.js
+++ electron/start.js
@@ -2,6 +2,21 @@ const { app, BrowserWindow } = require('electron');
 
 app.on('ready', handleAppReady);
 
+// declare it here so it is not garbage collected
+let mainWindow = null;
+
 function handleAppReady() {
-  const mainWindow = new BrowserWindow();
+  // show false - means hide the main window initially
+  mainWindow = new BrowserWindow({ show: false });
+
+  // load the html
+  // mainWindow.loadFile(`${__dirname}/index.html`);
+  mainWindow.loadURL('http://localhost:8080/');
+
+  // the html is loaded so the browser window can be shown
+  mainWindow.once('ready-to-show', renderApp);
+}
+
+function renderApp() {
+  mainWindow.show();
 }
