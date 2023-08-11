7c8c9a23344fed595ceba428d54d009d7ca44e10
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-08-11 17:50:43 +0200

    electron process: define logger creator

--- electron/logger.js
+++ electron/logger.js
@@ -1,13 +1,17 @@
 const winston = require('winston');
 const moment = require('moment');
+const path = require('node:path');
 
-const now = moment().format('YYYYMMDD-HHmmss');
+const now = moment().format('YYYYMMDD');
+// const now = moment().format('YYYYMMDD-HHmmss');
 
-function createLogger() {
+function createLogger(name) {
+  const directory = path.join('logs', name, `${now}.log`);
+
   return winston.createLogger({
     transports: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: `logs/${now}.log` })
+      new winston.transports.File({ filename: directory })
     ]
   });
 }
