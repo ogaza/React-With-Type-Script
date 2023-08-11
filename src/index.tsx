9e82fc7667aac07f8a66e96f5e2730a40096c7a0
Olaf Gaza <olaf.gaza@dieboldnixdorf.com>
2023-08-11 17:52:23 +0200

    code cleaning in main file

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './application/components/App';
 import { actions as appActions } from './application/actions/actions';
-// import { ItemActions } from './items';
+import App from './application/components/App';
+import store from './application/store/store';
 import { registerListeners } from './socket/listeners';
 import { socket } from './socket/socket';
-
-import store from './application/store/store';
 
 registerListeners(store, socket);
 
 const action = appActions.start();
 
 store.dispatch(action);
-// store.dispatch(ItemActions.getItems());
 
 ReactDOM.render(
   <Provider store={store}>
