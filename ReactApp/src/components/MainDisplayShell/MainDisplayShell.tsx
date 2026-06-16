dfd31e1fea85d8874154c6d0473d45f10be4b677
c-ogaza <olaf.gaza@outlook.com>
2026-06-16 12:37:05 +0200

    react_with_redux_toolkit: basic layout and styling reorganization

--- ReactApp/src/components/MainDisplayShell/MainDisplayShell.tsx
+++ ReactApp/src/components/MainDisplayShell/MainDisplayShell.tsx
@@ -1,14 +1,26 @@
-import { ClickableArea } from '../Buttons/ClickableArea/ClickableArea';
+import { ClickableArea } from "../Buttons/ClickableArea/ClickableArea";
 
 export default function MainDisplayShell() {
-  const className = ['main-display-shell'].join(' ');
+  const className = ["main-display-shell"].join(" ");
 
   return (
-    <div className={className}>
-      <h1>New React/Vite App</h1>
-      <div className="main">
-        <ClickableArea />
+    <div className="app-container">
+      <section className="app-header">
+        <NavBar />
+      </section>
+      <div className="panel-container">
+        <section className="panel panel__left"></section>
+        <section className="panel panel__right">
+          {/* <section className="panel panel__right" style={{"visibility": "hidden" }}> */}
+          <ClickableArea />
+        </section>
       </div>
     </div>
+  );
+}
+
+export function NavBar() {
+  return (
+      <h1>New React App</h1>
   );
 }
