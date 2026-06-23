d00dd5491aac1cdea8e5dbb9022a303d81552e4d
c-ogaza <olaf.gaza@outlook.com>
2026-06-23 11:11:34 +0200

    react_with_redux_toolkit: subscribe to store change

--- AppState/src/app.ts
+++ AppState/src/app.ts
@@ -1,6 +1,15 @@
 import { add, store } from "./store";
 
+const unsubscribe = store.subscribe(handleStoreChanged);
+
 console.log(store.getState());
 
 store.dispatch(add(2));
 console.log(store.getState());
+
+unsubscribe();
+
+function handleStoreChanged() {
+  console.log("store changed to: ", store.getState());
+  // console.log("store changed to: ", store.getState().value);
+}
